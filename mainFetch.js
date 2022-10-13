ff0b8da8e2fe6c2b4b8e3922cf77dd2d121098bd
rodrigo-28 <rodrigoromano2019@gmail.com>
2022-10-13 11:15:05 -0300

    crudTerminado

--- mainFetch.js
+++ mainFetch.js
@@ -6,11 +6,7 @@ $form= d.querySelector(".crud-form"),
 $title=d.querySelector(".crud-title"),
 $template=d.getElementById("crud-template").content,
 $fragment=d.createDocumentFragment();
-// console.log($table);
-// console.log($form);
-// console.log($title);
-// console.log($template);
-// console.log($fragment);
+
 
 const url="http://localhost:3000/Empresa/";
 const getAll=async()=>{
@@ -26,6 +22,7 @@ const getAll=async()=>{
         $template.querySelector(".edit").dataset.name=el.nombre;
         $template.querySelector(".edit").dataset.impuesto=el["valor de impuesto"];
         $template.querySelector(".delete").dataset.id=el.id;
+        $template.querySelector(".delete").dataset.name=el.nombre;
 
 
         let $clone=d.importNode($template,true);
@@ -63,12 +60,61 @@ d.addEventListener("submit",async e=>{
 
                     
                 };
-                console.log(option.body)
                 let res=await helpHttp().Post(`${url}`,option)
-               // location.reload();
+               location.reload();
                 
-            } catch (error) {
+            } catch (err) {
                 $table.insertAdjacentHTML("afterend",`<p><b>${err.status}:${err.statusText}</b></p>`);
+
+            }
+        }else{
+            try {
+                let option={
+                    method:"PUT",
+                    headers:{
+                        "Content-type":"application/json;charset=utf-8",
+                    },
+                    body:{
+                        nombre:e.target.nombre.value,
+                        ["valor de impuesto"]:e.target.impuesto.value
+                    }
+                    
+                }
+                let res =await helpHttp().Put(`${url}${e.target.id.value}`,option);
+                location.reload();
+                
+            } catch (err) {
+                $table.insertAdjacentHTML("afterend",`<p><b>${err.status}:${err.statusText}</b></p>`);
+
+            }
+        }
+    }
+});
+
+d.addEventListener("click",async e=>{
+    if(e.target.matches(".edit")){
+        $title.textContent="editar empresa";
+        $form.nombre.value=e.target.dataset.name;
+        $form.impuesto.value=e.target.dataset.impuesto;
+        $form.id.value=e.target.dataset.id;
+
+    }
+    if(e.target.matches(".delete")){
+        alert("presionaste el boton de eliminar");
+        let isDelete=confirm(`Estas seguro que quieres eliminar la empresa :  ${e.target.dataset.name}`);
+        if(isDelete){
+            try {
+                let option={
+                    method:"DELETE",
+                    headers:{
+                        "Content-type":"aplication/json;charset=utf-8"
+                    }
+                }
+                let res= await helpHttp().Del(`${url}${e.target.dataset.id}`,option);
+                location.reload();
+
+            } catch (err) {
+              $table.insertAdjacentHTML("afterend",`<p><b>${err.status}:${err.statusText}</b></p>`);
 
             }
         }
